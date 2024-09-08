719ac1479dbfeff4bb5c28c39f0ef9f86ed7646d
christoffer-ridderland <christoffer.ridderland@gmail.com>
2024-09-08 16:07:50 +0200

    added print board function

--- src/misc/misc.c
+++ src/misc/misc.c
@@ -1,4 +1,16 @@
-#ifndef CHMISC_H
-#define CHMISC_H
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+#include <stdint.h>
 
-#endif
+#include "misc.h"
+
+//void printbf(char* before, int64_t board) {
+//    printf(before)
+//    char cur[64 + 8];
+//    memset(cur, ' ', sizeof(cur));
+//    for (int i = 63; i >= 0; i--) {
+//        // TODO fix print
+//    }
+//}
---
+++ src/misc/misc.h
@@ -0,0 +1,6 @@
+#ifndef MISC_H
+#define MISC_H
+
+void printfb(int64_t subboard);
+
+#endif
